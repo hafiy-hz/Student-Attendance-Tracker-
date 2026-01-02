aacc62b360999be31e7bd8d10702529eeef84ab9
Adam <adamsyafiqsallehuddin@gmail.com>
2026-01-03 00:59:29 +0800

    name

--- AttendanceTracker.cpp
+++ AttendanceTracker.cpp
@@ -4,12 +4,12 @@
 // Lecture Class: TC5L
 // Tutorial Class: TT15L
 // Trimester: 2530
-// Member_1: 252UC242D2 | ADAM SYAFIQ BIN SALLEHUDDIN | ADAM.SYAFIQ.SALLEHUDDIN@STUDENT.MMU.EDU.MY | 012-361 3065
+// Member_1: 252UC242D2 | ADAM SYAFIQ BIN SALLEHUDDIN | ADAM.SYAFIQ.SALLEHUDDIN1@STUDENT.MMU.EDU.MY | 012-361 3065
 // Member_2: 252UC242QY | MUHAMMAD HAFIY AL-HAFIIZH BIN JOAHARI | MUHAMMAD.HAFIY.ALHAFIIZH1@STUDENT.MMU.EDU.MY | 016-212 3859
 // Member_3: 252UC242NE | AIDAN KHALIQ BIN SHAHNAZARUDDIN | AIDAN.KHALIQ.SHAHNZARUDDI1@student.mmu.edu.my | 017-615 5250
 // *********************************************************
 // Task Distribution
-// Member_1:
+// Member_1: Implemented the CSV viewer for the program, display.
 // Member_2: Implemented the main program structure, insertRow.
 // Member_3:
 // *********************************************************
